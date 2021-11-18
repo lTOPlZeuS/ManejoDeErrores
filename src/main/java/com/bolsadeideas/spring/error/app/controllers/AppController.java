74b8771f3e4b932ac74f2585935709ebbf7e47af
Jesus <jesus_reddead@comunidad.unam.mx>
2021-11-17 19:50:44 -0600

    Agregando primeras pag de valdacion 500 and 404

---
+++ src/main/java/com/bolsadeideas/spring/error/app/controllers/AppController.java
@@ -0,0 +1,15 @@
+package com.bolsadeideas.spring.error.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class AppController {
+  @GetMapping("/index")
+  public String index() {
+    Integer valor = 100/0;
+    return "index";
+  }
+  
+}
